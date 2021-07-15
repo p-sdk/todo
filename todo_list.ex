edbf7962ee88debf80bb3bfb30a7b2d75fd34b9b
p-sdk <paul.sdk@icloud.com>
2021-07-15 14:26:14 +0200

    Extract MultiDict

--- todo_list.ex
+++ todo_list.ex
@@ -1,17 +1,24 @@
 defmodule TodoList do
-  def new, do: %{}
+  def new, do: MultiDict.new()
 
   def add_entry(todo_list, date, title) do
-    Map.update(
-      todo_list,
-      date,
-      [title],
-      fn titles -> [title | titles] end
-    )
+    MultiDict.add(todo_list, date, title)
   end
 
   def entries(todo_list, date) do
-    Map.get(todo_list, date, [])
+    MultiDict.get(todo_list, date)
+  end
+end
+
+defmodule MultiDict do
+  def new, do: %{}
+
+  def add(dict, key, value) do
+    Map.update(dict, key, [value], &[value | &1])
+  end
+
+  def get(dict, key) do
+    Map.get(dict, key, [])
   end
 end
 
