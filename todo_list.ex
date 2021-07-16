1ad7d67ceb260f37a239a7d4cf0ebf9eb9b9806d
p-sdk <paul.sdk@icloud.com>
2021-07-16 22:42:38 +0200

    Represent entries with maps

--- todo_list.ex
+++ todo_list.ex
@@ -1,8 +1,8 @@
 defmodule TodoList do
   def new, do: MultiDict.new()
 
-  def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title)
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
   end
 
   def entries(todo_list, date) do
@@ -30,11 +30,13 @@ defmodule TodoListTest do
   test "TodoList" do
     todo_list =
       TodoList.new()
-      |> TodoList.add_entry(~D[2018-12-19], "Dentist")
-      |> TodoList.add_entry(~D[2018-12-20], "Shopping")
-      |> TodoList.add_entry(~D[2018-12-19], "Movies")
+      |> TodoList.add_entry(%{date: ~D[2018-12-19], title: "Dentist"})
+      |> TodoList.add_entry(%{date: ~D[2018-12-20], title: "Shopping"})
+      |> TodoList.add_entry(%{date: ~D[2018-12-19], title: "Movies"})
 
-    assert ["Movies", "Dentist"] == TodoList.entries(todo_list, ~D[2018-12-19])
+    assert [%{date: ~D[2018-12-19], title: "Movies"}, %{date: ~D[2018-12-19], title: "Dentist"}] ==
+             TodoList.entries(todo_list, ~D[2018-12-19])
+
     assert [] == TodoList.entries(todo_list, ~D[2018-12-18])
   end
 end
