e8ea6eb7414b8f9804ed25d1b625aefd8abb5397
p-sdk <paul.sdk@icloud.com>
2021-07-19 21:02:13 +0200

    Autogenerate IDs for entries

--- todo_list.ex
+++ todo_list.ex
@@ -1,24 +1,19 @@
 defmodule TodoList do
-  def new, do: MultiDict.new()
+  defstruct auto_id: 1, entries: %{}
+
+  def new, do: %TodoList{}
 
   def add_entry(todo_list, entry) do
-    MultiDict.add(todo_list, entry.date, entry)
+    entry = Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
   end
 
   def entries(todo_list, date) do
-    MultiDict.get(todo_list, date)
-  end
-end
-
-defmodule MultiDict do
-  def new, do: %{}
-
-  def add(dict, key, value) do
-    Map.update(dict, key, [value], &[value | &1])
-  end
-
-  def get(dict, key) do
-    Map.get(dict, key, [])
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
   end
 end
 
@@ -34,7 +29,7 @@ defmodule TodoListTest do
       |> TodoList.add_entry(%{date: ~D[2018-12-20], title: "Shopping"})
       |> TodoList.add_entry(%{date: ~D[2018-12-19], title: "Movies"})
 
-    assert [%{date: ~D[2018-12-19], title: "Movies"}, %{date: ~D[2018-12-19], title: "Dentist"}] ==
+    assert [%{date: ~D[2018-12-19], title: "Dentist"}, %{date: ~D[2018-12-19], title: "Movies"}] =
              TodoList.entries(todo_list, ~D[2018-12-19])
 
     assert [] == TodoList.entries(todo_list, ~D[2018-12-18])
