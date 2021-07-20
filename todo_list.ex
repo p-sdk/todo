927331fc91c2c7feae5819b7f15afdb9b5f96526
p-sdk <paul.sdk@icloud.com>
2021-07-20 10:04:14 +0200

    Allow to update entries

--- todo_list.ex
+++ todo_list.ex
@@ -15,6 +15,23 @@ defmodule TodoList do
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
     |> Enum.map(fn {_, entry} -> entry end)
   end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
 end
 
 ExUnit.start()
@@ -33,5 +50,10 @@ defmodule TodoListTest do
              TodoList.entries(todo_list, ~D[2018-12-19])
 
     assert [] == TodoList.entries(todo_list, ~D[2018-12-18])
+
+    assert [%{date: ~D[2018-12-21], title: "Dentist"}] =
+             todo_list
+             |> TodoList.update_entry(1, fn entry -> %{entry | date: ~D[2018-12-21]} end)
+             |> TodoList.entries(~D[2018-12-21])
   end
 end
