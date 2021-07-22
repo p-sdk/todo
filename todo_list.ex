692ad7e6bd0c45aead2a2327c05f0a5c3a056443
p-sdk <paul.sdk@icloud.com>
2021-07-22 13:03:46 +0200

    Add a basic TodoServer

--- todo_list.ex
+++ todo_list.ex
@@ -1,3 +1,41 @@
+defmodule TodoServer do
+  def start do
+    spawn(fn -> loop(TodoList.new()) end)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    send(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    send(todo_server, {:entries, self(), date})
+
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  defp loop(todo_list) do
+    new_todo_list =
+      receive do
+        message -> process_message(todo_list, message)
+      end
+
+    loop(new_todo_list)
+  end
+
+  defp process_message(todo_list, {:add_entry, new_entry}) do
+    TodoList.add_entry(todo_list, new_entry)
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+    todo_list
+  end
+end
+
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
@@ -102,6 +140,23 @@ defmodule TodoList.CsvImporter do
 end
 
 ExUnit.start()
+
+defmodule TodoServerTest do
+  use ExUnit.Case, async: true
+
+  test "TodoServer" do
+    todo_server = TodoServer.start()
+
+    TodoServer.add_entry(todo_server, %{date: ~D[2018-12-19], title: "Dentist"})
+    TodoServer.add_entry(todo_server, %{date: ~D[2018-12-20], title: "Shopping"})
+    TodoServer.add_entry(todo_server, %{date: ~D[2018-12-19], title: "Movies"})
+
+    assert [%{date: ~D[2018-12-19], title: "Dentist"}, %{date: ~D[2018-12-19], title: "Movies"}] =
+             TodoServer.entries(todo_server, ~D[2018-12-19])
+
+    assert [] == TodoServer.entries(todo_server, ~D[2018-12-18])
+  end
+end
 
 defmodule TodoListTest do
   use ExUnit.Case, async: true
